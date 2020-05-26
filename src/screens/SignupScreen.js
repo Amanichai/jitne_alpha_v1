36915863887f64bc8fbd1966324b207713692c00
Amanichai <amanichai@gmail.com>
2020-05-26 01:11:16 -0400

    add AuthForm and basic styling to SignupScreen. Add Spacer to components

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
 
 const SignupScreen = ()=>{
     return <View style={styles.container}>
-                <Text>SignupScreen</Text>
+                <AuthForm
+                    headerText='Sign Up To Jitne'
+                    errorMsg=""
+                    onSubmit={()=>{}}
+                    submitButtonText='Sign Up'
+                ></AuthForm>
             </View>
 };
 
-const styles = StyleSheet.create({
+SignupScreen.navigationOptions = ()=>{
+    return {
+        headerShown: false
+    }
+}
 
+const styles = StyleSheet.create({
+    container: {
+        textAlign: "center",
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
 });
 
 export default SignupScreen;
